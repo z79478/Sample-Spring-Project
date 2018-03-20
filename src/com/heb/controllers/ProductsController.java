afa233266bd5742f1f88d49c62b9912f3ccf9a5a
z79478 <37587425+z79478@users.noreply.github.com>
2018-03-20 18:00:17 -0500

    next update

--- src/com/heb/controllers/ProductsController.java
+++ src/com/heb/controllers/ProductsController.java
@@ -1,15 +1,41 @@
 package com.heb.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.heb.models.ProductsModel;
+import com.heb.services.ProductsService;
+
 
 @Controller
 @RequestMapping("products")
 @SessionAttributes("ProductsModel")
 public class ProductsController {
 
+	@Autowired
+	ProductsService productService;
 	
 	
+	@RequestMapping(method = RequestMethod.GET)
+	public String get(Model model) {
+		ProductsModel prodModel = new ProductsModel();
+		
+		model.addAttribute("ProductsModel", prodModel);
+		return "products";
+	}
+	
+	@RequestMapping(value="/retrieve", method = RequestMethod.POST)
+	public String retrieveProducts(@ModelAttribute("ProductsModel") ProductsModel prodModel, BindingResult result) {
+		
+		prodModel.setProducts(productService.getProducts(prodModel.getProdFilter()));
+		
+		return "products";
+	}
 	
 }
--- src/net/esc20/txeis/sample/controllers/AjaxController.java
+++
@@ -1,8 +0,0 @@
-package net.esc20.txeis.sample.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class AjaxController {
-
-}
